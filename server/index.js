b5fbfc7c122cdd1294cf4f76dcbfdba4e0a4f9bd
adriandurran <adriandurran@users.noreply.github.com>
2018-11-19 14:34:56 +0000

    feature: scaffolding out the graphql auth

--- server/index.js
+++ server/index.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 
+const keys = require('./config/keys');
+
 const morgan = require('morgan');
+
+require('./models/tech');
+require('./services/passport');
+
+// connect to db
+mongoose
+  .connect(
+    keys.mongoURI,
+    { useNewUrlParser: true, poolSize: 10 }
+  )
+  .then(
+    () => {
+      console.log('Database conenction successful');
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(
+  cookieSession({
+    name: 'accmanager',
+    maxAge: 8 * 60 * 60 * 1000, // 8 hrs while in dev
+    keys: [keys.cookieKey]
+  })
+);
 
 app.use(
   '/graphql',
@@ -17,6 +45,11 @@ app.use(
     graphiql: true
   })
 );
+
+// passport for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(morgan('dev'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
